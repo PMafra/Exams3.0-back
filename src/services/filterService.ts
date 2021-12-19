50920af08a7e8f984edbaba39a44fd4346518ac3
PMafra <pmafrarm@gmail.com>
2021-12-19 18:21:35 -0300

    Finish adding queryBuilders to filterService

--- src/services/filterService.ts
+++ src/services/filterService.ts
@@ -1,7 +1,7 @@
 /* eslint-disable max-len */
 /* eslint-disable @typescript-eslint/no-explicit-any */
 /* eslint-disable no-console */
-import { getManager, getRepository } from 'typeorm';
+import { getRepository } from 'typeorm';
 import SchoolEntity from '../entities/schools';
 import CategoryEntity from '../entities/categories';
 import { selector } from '../infra/selectors';
@@ -19,20 +19,16 @@ async function obtainCategories() {
   return categoriesList;
 }
 
-function getUniqueList(arr: any[]) {
+function getUniqueListById(arr: any[]) {
   return [...new Map(arr?.slice().reverse().map((key: any) => [key.id, key])).values()].reverse();
 }
 
 async function obtainProfessorsByFilter(filters: any) {
   const school = filters?.school;
   const subject = filters?.subject;
-  //   professorsList = await getManager().query(`
-  //   SELECT professors.professor, professors.id FROM professors_subjects_schools JOIN professors ON professors.id = professors_subjects_schools.professor_id JOIN schools ON schools.id = professors_subjects_schools.school_id WHERE schools.school = $1;
-  // `, [school]);
-
   // professorsList = await getRepository(ProfessorSubjectSchoolEntity).find({
   //   select: [
-  //     professor:,
+  //     'professor',
   //   ],
   //   where: {
   //     school: { school },
@@ -49,9 +45,6 @@ async function obtainProfessorsByFilter(filters: any) {
       .select(['professor.professor AS professor', 'professor.id AS id'])
       .getRawMany();
   }
-  //   professorsList = await getManager().query(`
-  //   SELECT professors.professor, professors.id FROM professors_subjects_schools JOIN professors ON professors.id = professors_subjects_schools.professor_id JOIN schools ON schools.id = professors_subjects_schools.school_id JOIN subjects ON subjects.id = professors_subjects_schools.subject_id WHERE schools.school = $1 AND subjects.subject = $2;
-  // `, [school, subject]);
   if (school && subject) {
     professorsList = await getRepository(ProfessorSubjectSchoolEntity)
       .createQueryBuilder('professorSubjectSchool')
@@ -64,7 +57,7 @@ async function obtainProfessorsByFilter(filters: any) {
       .getRawMany();
   }
 
-  const uniqueProfessorsList = getUniqueList(professorsList);
+  const uniqueProfessorsList = getUniqueListById(professorsList);
 
   return uniqueProfessorsList;
 }
@@ -72,14 +65,17 @@ async function obtainProfessorsByFilter(filters: any) {
 async function obtainSubjectsByFilter(filters: any) {
   const school = filters?.school;
   let subjectsList;
-
   if (school) {
-    subjectsList = await getManager().query(`
-    SELECT subjects.subject, subjects.id FROM professors_subjects_schools JOIN subjects ON subjects.id = professors_subjects_schools.subject_id JOIN schools ON schools.id = professors_subjects_schools.school_id WHERE schools.school = $1;
-  `, [school]);
+    subjectsList = await getRepository(ProfessorSubjectSchoolEntity)
+      .createQueryBuilder('professorSubjectSchool')
+      .where('school.school = :school', { school })
+      .leftJoin('professorSubjectSchool.subject', 'subject')
+      .leftJoin('professorSubjectSchool.school', 'school')
+      .select(['subject.subject AS subject', 'subject.id AS id'])
+      .getRawMany();
   }
 
-  const uniqueSubjectsList = getUniqueList(subjectsList);
+  const uniqueSubjectsList = getUniqueListById(subjectsList);
 
   return uniqueSubjectsList;
 }
