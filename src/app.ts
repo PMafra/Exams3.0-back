172870d771ea839cba3cb90da994855462480022
PMafra <pmafrarm@gmail.com>
2021-12-20 16:47:08 -0300

    Update cors options

--- src/app.ts
+++ src/app.ts
@@ -9,7 +9,11 @@ import 'reflect-metadata';
 import connectDatabase from './dbconfig';
 
 const app = express();
-app.use(cors());
+app.use((req, res) => {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
+  app.use(cors());
+});
 app.use(express.json());
 
 app.use(middlewareInfo);
