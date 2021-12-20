a98b6ec7a60cbf474d485de3eeae4ec3597715fe
PMafra <pmafrarm@gmail.com>
2021-12-20 19:24:31 -0300

    Fix server port

--- src/server.ts
+++ src/server.ts
@@ -2,7 +2,7 @@ import './setup';
 import app, { init } from './app';
 import { logger } from './utils/logger';
 
-const port = +process.env.PORT || 4000;
+const port = process.env.PORT || 4000;
 
 init().then(() => {
   app.listen(port, () => {
